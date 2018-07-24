5d45b54ac2cdd2f87333ff33b9ed98b8088d4b1d
Abhishek Yadav <abhishekyadav@Abhisheks-MacBook-Pro.local>
2018-07-23 23:12:36 -0700

    till half 9

---
+++ 9_notes-node-workingwithJSON/playground/json.js
@@ -0,0 +1,19 @@
+// var obj = {
+//   name: 'Abhishek'
+// };
+//
+// var stringObj = JSON.stringify(obj);
+//
+// console.log(typeof stringObj); //output of this is string
+// console.log(stringObj); //output of this is name: 'Abhishek'
+
+
+// var personString = '{"name": "Abhishek","age": 23}';
+// //Since it is a string we need to make it an object
+//
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+
+//ANOTHER METHOD
